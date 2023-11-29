922b787a71fcc8bb2e29651ff36f7a1446635902
jalqueza <alqueza@ualberta.ca>
2023-11-29 11:29:55 -0800

    lab 9

--- src/app/dealer-inventory/dealer-inventory.component.ts
+++ src/app/dealer-inventory/dealer-inventory.component.ts
@@ -1,73 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Vehicle } from '../vehicle'
+import {InventoryService} from '../inventory.service'
 
 @Component({
   selector: 'app-dealer-inventory',
   templateUrl: './dealer-inventory.component.html',
   styleUrls: ['./dealer-inventory.component.css']
 })
-export class DealerInventoryComponent {
+export class DealerInventoryComponent implements OnInit {
   vehicleToEdit?:Vehicle
-  beginEditing(v:Vehicle) {
-    this.vehicleToEdit = v
-  }
-  
-  commitEdit(v:Vehicle) {
-    //Copy the edited data
-    Object.assign(this.vehicleToEdit!, v)
-  
-    this.vehicleToEdit = undefined
-  }
-  inventory:Vehicle[] = [
-    {
-      VIN: "Y123",
-      year: 2012,
-      make: "HONDA",
-      model: "Civic",
-      mileage: 70000,
-      price: 5900.00,
-      featured: false,
-      photos: []
-    },
-    {
-      VIN: "P1023",
-      year: 2019,
-      make: "BMW",
-      model: "328i",
-      mileage: 42000,
-      price: 12000.00,
-      featured: true,
-      photos: ["/assets/b-1.png", "/assets/b-2.png", "/assets/b-3.png", "/assets/b-4.png"]
-    },
-    {
-      VIN: "NM182",
-      year: 2018,
-      make: "KIA",
-      model: "Niro",
-      mileage: 31000,
-      price: 7900.00,
-      featured: false,
-      photos: ["/assets/k-1.png", "/assets/k-2.png", "/assets/k-3.png"]
 
-    },
-    {
-      VIN: "Y187",
-      year: 2014,
-      make: "HONDA",
-      model: "Accord",
-      mileage: 40000,
-      price: 8900.00,
-      featured: false,
-      photos: []
-    },
-  ]
+  inventory:Vehicle[] = []
+  constructor(private inventorySvc:InventoryService) { }
+  ngOnInit(): void {
+    this.inventory = this.inventorySvc.getInventory()
+  }
 
   trackByVIN(index:number, car:Vehicle) : string {
     return car.VIN
   }
 
   deleteVehicle(car:Vehicle) {
-    this.inventory = this.inventory.filter(c => c.VIN != car.VIN)
+    this.inventorySvc.deleteVehicle(car)
+  this.inventory = this.inventorySvc.getInventory()
   }
 
   handlePhotoNavigation(photoIndex:number, car:Vehicle) {
@@ -75,8 +30,18 @@ export class DealerInventoryComponent {
       alert("Come visit us in our showroom!")
     }
   }
-
   addVehicle(v:Vehicle) {
-    this.inventory.push(v)
+    this.inventorySvc.addVehicle(v)
+  this.inventory = this.inventorySvc.getInventory()
+  }
+  beginEditing(v:Vehicle) {
+    this.vehicleToEdit = v
+  }
+  
+  commitEdit(v:Vehicle) {
+    this.inventorySvc.updateVehicle(this.vehicleToEdit!.VIN, v)
+  this.inventory = this.inventorySvc.getInventory()
+  
+    this.vehicleToEdit = undefined
   }
 }
