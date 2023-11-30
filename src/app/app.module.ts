8e80a4f18040d13af90152f175dd25b814d4213e
jalqueza <alqueza@ualberta.ca>
2023-11-30 12:12:56 -0800

    lab 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+import '@angular/common/locales/global/fr';
 
 import { AppComponent } from './app.component';
 import { TapCounterComponent } from './tap-counter/tap-counter.component';
