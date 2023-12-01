66c154c200becf1e0eaeba6636a68b56edc36226
jalqueza <alqueza@ualberta.ca>
2023-12-01 11:18:26 -0800

    lab 16

---
+++ large-app/src/app/account/account-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AccountHomeComponent } from './account-home/account-home.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { AddressbookComponent } from './addressbook/addressbook.component';
+
+const routes: Routes = [
+{path: "", component: AccountHomeComponent},
+{path: "orders", component: OrderHistoryComponent},
+{path: "address", component: AddressbookComponent}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AccountRoutingModule { }
